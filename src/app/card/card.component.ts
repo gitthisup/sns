6e1643b453ebc7fc403c5fecfecfc29e6949e095
gitthisup <gitthisup>
2023-02-20 00:08:34 +0100

    ok

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, Pipe, PipeTransform } from '@angular/core';
 import { CardModel } from '../models/card-model';
 
+
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
